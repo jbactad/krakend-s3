ee07d3a2913a0a7f4d31ba5b9aa330e570476734
Jim Geoshua Bactad <jim.bactad@talabat.com>
2022-11-16 20:56:34 +0400

    Validate s3 bucket name

--- backend.go
+++ backend.go
@@ -1,20 +1,78 @@
-package krakends3
+package s3
 
 import (
 	"context"
+	"errors"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/luraproject/lura/v2/config"
 	"github.com/luraproject/lura/v2/logging"
 	"github.com/luraproject/lura/v2/proxy"
 )
 
+var (
+	errNoConfig      = errors.New("aws s3: no extra config defined")
+	errInvalidBucket = errors.New(`aws s3: invalid "bucket" defined`)
+	errInvalidConfig = errors.New("aws s3: invalid config")
+)
+
+const Namespace = "github.com/jbactad/krakend-s3"
+
+type Options struct {
+	AWS *aws.Config
+}
+
 func BackendFactory(logger logging.Logger, bf proxy.BackendFactory) proxy.BackendFactory {
 	return func(remote *config.Backend) proxy.Proxy {
-		if _, ok := remote.ExtraConfig["github.com/jbactad/krakend-s3"]; !ok {
+		logPrefix := "[BACKEND: " + remote.URLPattern + "][S3]"
+		_, err := getOptions(remote)
+		if err != nil {
+			if err != errNoConfig {
+				logger.Error(logPrefix, err)
+			}
+
 			return bf(remote)
 		}
+
+		if _, ok := remote.ExtraConfig[Namespace]; !ok {
+			logger.Error(logPrefix, errNoConfig)
+			return bf(remote)
+		}
+
 		return func(ctx context.Context, request *proxy.Request) (*proxy.Response, error) {
 			return nil, nil
 		}
 	}
+}
+
+func getOptions(remote *config.Backend) (*Options, error) {
+	v, ok := remote.ExtraConfig[Namespace]
+	if !ok {
+		return nil, errNoConfig
+	}
+
+	cfg, ok := v.(map[string]interface{})
+	if !ok {
+		return nil, errInvalidConfig
+	}
+
+	v, ok = cfg["bucket"]
+	if !ok {
+		return nil, errInvalidBucket
+	}
+
+	key, ok := v.(string)
+	if !ok {
+		return nil, errInvalidBucket
+	}
+
+	if key == "" {
+		return nil, errInvalidBucket
+	}
+
+	opts := &Options{
+
+	}
+
+	return opts, nil
 }
--- backend_test.go
+++ backend_test.go
@@ -1,63 +1,169 @@
-package krakends3_test
+package s3_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 
-	krakends3 "github.com/jbactad/krakend-s3"
+	"github.com/golang/mock/gomock"
+	s3 "github.com/jbactad/krakend-s3"
+	"github.com/jbactad/krakend-s3/mocks"
 	"github.com/luraproject/lura/v2/config"
-	"github.com/luraproject/lura/v2/logging"
 	"github.com/luraproject/lura/v2/proxy"
 	"github.com/stretchr/testify/assert"
 )
 
+//go:generate mockgen -destination mocks/lura_logger.go -package mocks github.com/luraproject/lura/v2/logging Logger
+
 func TestBackendFactory_invalidConfig(t *testing.T) {
-	l := logging.NoOp
+	ctrl := gomock.NewController(t)
+	l := mocks.NewMockLogger(ctrl)
+
 	oResp := &proxy.Response{
 		Data:       map[string]interface{}{},
 		IsComplete: true,
 		Metadata:   proxy.Metadata{},
 		Io:         nil,
 	}
-	bf := proxy.BackendFactory(
-		func(remote *config.Backend) proxy.Proxy {
-			return func(ctx context.Context, request *proxy.Request) (*proxy.Response, error) {
-				return oResp, nil
-			}
-		},
-	)
 
-	type fields struct {
-		logger logging.Logger
-	}
 	type args struct {
 		config *config.Backend
 	}
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
 		wantErr assert.ErrorAssertionFunc
 		want    *proxy.Response
+		setup   func(logger *mocks.MockLogger)
 	}{
 		{
-			name: "namespace not found in config, should invoke original proxy",
-			fields: fields{
-				logger: l,
-			},
+			name: "namespace not found in config, should return original proxy",
 			args: args{
 				config: &config.Backend{
+					URLPattern:  "/some-endpoint",
 					ExtraConfig: map[string]interface{}{},
 				},
 			},
 			wantErr: assert.NoError,
 			want:    oResp,
 		},
+		{
+			name: "invalid config for namespace, should log error and return original proxy",
+			args: args{
+				config: &config.Backend{
+					URLPattern: "/some-endpoint",
+					ExtraConfig: map[string]interface{}{
+						s3.Namespace: "",
+					},
+				},
+			},
+			wantErr: assert.NoError,
+			want:    oResp,
+			setup: func(logger *mocks.MockLogger) {
+				logger.EXPECT().Error(
+					"[BACKEND: /some-endpoint][S3]",
+					errors.New("aws s3: invalid config"),
+				)
+			},
+		},
+		{
+			name: "s3 bucket not defined, should log error and return original proxy",
+			args: args{
+				config: &config.Backend{
+					URLPattern: "/some-endpoint",
+					ExtraConfig: map[string]interface{}{
+						s3.Namespace: map[string]interface{}{},
+					},
+				},
+			},
+			wantErr: assert.NoError,
+			want:    oResp,
+			setup: func(logger *mocks.MockLogger) {
+				logger.EXPECT().Error(
+					"[BACKEND: /some-endpoint][S3]",
+					errors.New(`aws s3: invalid "bucket" defined`),
+				)
+			},
+		},
+		{
+			name: "s3 bucket empty, should log error and return original proxy",
+			args: args{
+				config: &config.Backend{
+					URLPattern: "/some-endpoint",
+					ExtraConfig: map[string]interface{}{
+						s3.Namespace: map[string]interface{}{
+							"bucket": "",
+						},
+					},
+				},
+			},
+			wantErr: assert.NoError,
+			want:    oResp,
+			setup: func(logger *mocks.MockLogger) {
+				logger.EXPECT().Error(
+					"[BACKEND: /some-endpoint][S3]",
+					errors.New(`aws s3: invalid "bucket" defined`),
+				)
+			},
+		},
+		{
+			name: "s3 bucket type invalid, should log error and return original proxy",
+			args: args{
+				config: &config.Backend{
+					URLPattern: "/some-endpoint",
+					ExtraConfig: map[string]interface{}{
+						s3.Namespace: map[string]interface{}{
+							"bucket": 1,
+						},
+					},
+				},
+			},
+			wantErr: assert.NoError,
+			want:    oResp,
+			setup: func(logger *mocks.MockLogger) {
+				logger.EXPECT().Error(
+					"[BACKEND: /some-endpoint][S3]",
+					errors.New(`aws s3: invalid "bucket" defined`),
+				)
+			},
+		},
+		{
+			name: "s3 bucket empty, should log error and return original proxy",
+			args: args{
+				config: &config.Backend{
+					URLPattern: "/some-endpoint",
+					ExtraConfig: map[string]interface{}{
+						s3.Namespace: map[string]interface{}{
+							"key":    "",
+							"secret": "some-secret",
+						},
+					},
+				},
+			},
+			wantErr: assert.NoError,
+			want:    oResp,
+			setup: func(logger *mocks.MockLogger) {
+				logger.EXPECT().Error(
+					"[BACKEND: /some-endpoint][S3]",
+					errors.New(`aws s3: invalid "bucket" defined`),
+				)
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(
 			tt.name, func(t *testing.T) {
-				b := krakends3.BackendFactory(tt.fields.logger, bf)
+				if tt.setup != nil {
+					tt.setup(l)
+				}
+
+				b := s3.BackendFactory(
+					l, func(remote *config.Backend) proxy.Proxy {
+						return func(ctx context.Context, request *proxy.Request) (*proxy.Response, error) {
+							return oResp, nil
+						}
+					},
+				)
 				p := b(tt.args.config)
 				got, err := p(nil, nil)
 				if !tt.wantErr(t, err) {
